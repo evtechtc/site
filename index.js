22b1a596385675dc609597ca44a7d8baf1d674e1
EVTECH CODER <105789673+evtech-coder@users.noreply.github.com>
2023-10-16 00:47:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const submit = document.getElementById('submit');
+
+
+let mail = document.getElementById("").innerText
+
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let ebody = `
+    <h1>First Name: </h1>${fname.value}
+    <br>
+    <h1>Last Name: </h1>${lname.value}
+    `;
+
+    Email.send({
+        SecureToken : "your-token", //add your token here
+        To : 'them@website.com', 
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : ebody
+    }).then(
+      message => alert(message)
+    );
+});
